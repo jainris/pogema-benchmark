3a144c474afc0558d2027135926310c6cc05fb19
Rishabh Jain <rj412@cam.ac.uk>
2025-01-17 19:30:08 +0000

    Added cross entropy ranking loss.

--- algorithms/ranking_losses.py
+++ algorithms/ranking_losses.py
@@ -71,6 +71,49 @@ class PairwiseDiffLoss(torch.nn.Module):
         return loss
 
 
+def get_ranking_from_relevance(relevance):
+    sorted_indices = torch.argsort(relevance, descending=True, stable=True)
+    ranks = torch.empty_like(relevance)
+
+    batch_indices = torch.arange(relevance.shape[0], device=relevance.device)
+    batch_indices = torch.unsqueeze(batch_indices, dim=-1)
+
+    ranks[batch_indices, sorted_indices] = torch.arange(
+        relevance.shape[-1], dtype=ranks.dtype
+    )
+
+    sorted_indices = torch.argsort(
+        torch.flip(relevance, dims=[-1]), descending=True, stable=True
+    )
+    ranks_flipped = torch.empty_like(relevance)
+    ranks_flipped[batch_indices, sorted_indices] = torch.arange(
+        relevance.shape[-1], dtype=ranks.dtype
+    )
+
+    ranks = (ranks + torch.flip(ranks_flipped, dims=[-1])) / 2
+
+    # Setting 0 relevance ranks to be inf
+    ranks[relevance == 0] = torch.inf
+    return ranks
+
+
+class RankingCrossEntropyLoss(torch.nn.Module):
+    def __init__(
+        self,
+        temperature: float = 1.0,
+    ):
+        super().__init__()
+        self.temperature = temperature
+
+    def forward(self, scores: Tensor, relevance: Tensor):
+        relevance = torch.argsort(relevance).to(scores.dtype)
+        target_rankings = get_ranking_from_relevance(relevance)
+        target_rankings = self.temperature * target_rankings
+        target_distribution = torch.nn.functional.softmax(-target_rankings, dim=-1)
+
+        return torch.nn.functional.cross_entropy(scores, target_distribution)
+
+
 def get_ranking_loss(pairwise_loss="logistic"):
     if pairwise_loss == "logistic":
         return PairwiseLogisticLoss()
@@ -80,6 +123,8 @@ def get_ranking_loss(pairwise_loss="logistic"):
         return PairwiseDiffLoss()
     elif pairwise_loss == "diff-softmax":
         return PairwiseDiffLoss(softmax_outputs=True)
+    elif pairwise_loss == "cross-entropy":
+        return RankingCrossEntropyLoss()
     else:
         raise ValueError(f"Unsupported ranking loss type: {pairwise_loss}.")
 
